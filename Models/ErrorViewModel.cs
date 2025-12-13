40477b9c2e3d0c53caaf662cd48b39d3b9658252
Nghia <nguyenphuocnghia@gmail.com>
2025-12-13 20:04:21 +0700

    chore: Init ASP.NET Core project and install dependencies

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileShareApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
